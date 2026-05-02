1ea4e470b4acbeefc8b5109eb833e35bc05a675f
Rajkumar Saravanakumar <rsrajkumar0097@gmail.com>
2026-05-03 01:43:58 +0530

    A4.2 Runner

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '96151353a1ff64934a4581290a120b7b736abb5b'
+AESD_ASSIGNMENTS_VERSION = 'c4336d7756c33e9dca1383519154efa860ea1557'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
